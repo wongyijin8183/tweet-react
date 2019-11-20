7ccc7c2cfd4fc2a880c2ad353f8aefcaf5d83178
wongyijin8183 <wongyijin8183@gmail.com>
2019-11-21 07:36:02 +0800

    basic display and alignment of posts

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import tweets from 'tweets'
 
 class App extends React.Component {
   render() {
+
+    let posts = tweets.map( post => {
+
+      let img_url = null;
+      if (post.entities.media) {
+        img_url = post.entities.media[0].media_url;
+
+      }
+
+      // let currentDate = Date.now()
+      // console.log("the date now is :",currentDate);
+
+      return (
+        <div>
+          <p><img src={post.user.profile_image_url} style={{borderRadius:"500px" , float:"left" , marginRight:"20px"}}/>{post.user.name.toUpperCase()} @{post.user.screen_name} .<br/>{post.text}</p>
+          
+          <p style={{textAlign:"right"}}><img src={img_url} style={{width:"80%" , borderRadius:"25px"}}/></p>
+          <hr/>
+        </div>
+      )
+    });
+
     return (
       <div>
-        tweets
+        {posts}
       </div>
-    );
-  }
+    )
+  }  
 }
+
 
 const element = document.getElementById('app');
 
